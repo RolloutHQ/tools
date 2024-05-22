3251cb1450b097d907dbf5f9e6d28f48172390d0
Jozef Mikuláš <jmikulas.sk@gmail.com>
2024-05-23 01:48:00 +0200

    feat: endpoint showcasing sync automation execution

--- rollout/apps/rollout-tools/http.ts
+++ rollout/apps/rollout-tools/http.ts
@@ -1,15 +1,60 @@
-import { defineHTTPHandler, handleConsumerRequest } from '@rollout/framework';
+import { Elysia, t } from 'elysia';
 
-export const http = defineHTTPHandler(async (request) => {
-  console.log('Handle request', request.url);
+import {
+  defineHTTPHandler,
+  handleConsumerRequest,
+  handlePushTriggerEvent,
+  waitForAutomationRunResult,
+  getAutomation
+} from '@rollout/framework';
 
-  const requestValidation = await handleConsumerRequest({
-    token: request.headers.get('authorization')?.replace('Bearer ', '') ?? '',
-  });
+const router = new Elysia()
+.post(
+  '/run-automation/:automationId',
+  async ({ body, params, request }) => {
+    const token = request.headers.get('authorization')?.replace('Bearer ', '');
+    
+    if (!token) {
+      throw new Response('No Authorization with Bearer token set', { status: 401 });
+    }
 
-  if (!requestValidation.ok) {
-    return Response.json(requestValidation.error, { status: 401 });
+    const requestValidation = await handleConsumerRequest({ token });
+
+    if (!requestValidation.ok) {
+      throw new Response(requestValidation.error, { status: 401 });
+    }
+
+    const automation = await getAutomation({
+      where: {
+        id: params.automationId,
+      },
+    });
+    
+    if (automation.consumerKey !== requestValidation.consumer.consumerKey) {
+      throw new Response("Automation not found", { status: 400 });
+    }
+
+    if (!automation.live) {
+      throw new Response("Automation is not live", { status: 400 });
+    }
+
+    const { automationRun } = await handlePushTriggerEvent({
+      automationId: params.automationId,
+      payload: body.payload,
+      addToQueueFront: true,
+    });
+
+    const { execution } = await waitForAutomationRunResult({
+      automationRunId: automationRun.id,
+    });
+
+    return { execution };
+  },
+  {
+    body: t.Object({
+      payload: t.Record(t.String(), t.Any()),
+    }),
   }
+);
 
-  return Response.json(`Hello from http to ${requestValidation.consumer.consumerKey}`);
-});
+export const http = defineHTTPHandler(router.handle);
